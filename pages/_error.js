3a1fa7f6c4daee95ef513e1f53b3164587f4857f
eduardo403 <edua.24h@gmail.com>
2023-02-24 15:45:58 +0000

    add api for github and pages

---
+++ pages/_error.js
@@ -0,0 +1,13 @@
+import Leyout from "../components/Leyout.js";
+
+const _error = () => {
+  return (
+    <>
+      <Leyout>
+        <p className="text-center"> Could not load your page</p>
+      </Leyout>
+    </>
+  );
+};
+
+export default _error;
--- pages/github.js
+++ pages/github.js
@@ -1,13 +1,41 @@
-import Leyout from "../components/Leyout";
-
-const github = () => {
+import Leyout from "../components/Leyout.js";
+import error from "./_error.js";
+const github = ({ user }) => {
+  console.log(user);
   return (
-    <div>
+    <>
       <Leyout>
-        <h2>github</h2>
+        <section className="row">
+          <div class="card col-md-4 offset-md-4" style={{ width: "28rem;" }}>
+            <img src={user.avatar_url} class="card-img-top" alt={user.name} />
+            <div class="card-body">
+              <h5 class="card-title">{user.name}</h5>
+              <p class="card-text">{user.bio}</p>
+              <a
+                href={user.html_url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="btn  btn-secondary"
+              >
+                Go to Github
+              </a>
+            </div>
+          </div>
+        </section>
       </Leyout>
-    </div>
+    </>
   );
 };
+
+export async function getServerSideProps() {
+  const res = await fetch("https://api.github.com/users/Eduardo403");
+  const json = await res.json();
+
+  return {
+    props: {
+      user: json,
+    },
+  };
+}
 
 export default github;
