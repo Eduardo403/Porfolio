2e2e56fad716034cff2ffe3598fa942d3cf44903
Eduardo <edua.24h@gmail.com>
2023-03-29 17:58:40 +0100

    add social media an from for contact

--- pages/contact.js
+++ pages/contact.js
@@ -4,7 +4,55 @@ const contact = () => {
   return (
     <>
       <Leyout>
-        <h2>contact</h2>
+        <form>
+          <fieldset>
+            <legend>contact me</legend>
+
+            <div class="form-group">
+              <label for="matter" class="form-label mt-4">
+                matter to be discussed
+              </label>
+              <input
+                type="text"
+                class="form-control"
+                id="matter"
+                aria-describedby="emailHelp"
+                placeholder="matter"
+              />
+            </div>
+
+            <div class="form-group">
+              <label for="exampleInputEmail1" class="form-label mt-4">
+                Email address
+              </label>
+              <input
+                type="email"
+                class="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+              />
+              <small id="emailHelp" class="form-text text-muted">
+                We'll never share your email with anyone else.
+              </small>
+            </div>
+
+            <div class="form-group">
+              <label for="exampleTextarea" class="form-label mt-4">
+                Example textarea
+              </label>
+              <textarea
+                class="form-control"
+                id="exampleTextarea"
+                rows="3"
+              ></textarea>
+            </div>
+            <hr />
+            <button type="submit" class="btn btn-primary">
+              Submit
+            </button>
+          </fieldset>
+        </form>
       </Leyout>
     </>
   );
